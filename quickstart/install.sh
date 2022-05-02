c79f599bca7c37155ff4ce64f5cf47a10b9846bf
Justin Henderson <jhenderson@tekrefresh.com>
2022-05-01 19:31:44 -0500

    a

--- quickstart/install.sh
+++ quickstart/install.sh
@@ -149,7 +149,8 @@ function pull_down_reflex_files {
   pull_file_if_missing "roles_mapping.yml"
 }
 function generate_random_password {
-  PASSWORD=$(cat /dev/urandom | tr -dc '[:alnum:]' | fold -w ${1:-25} | head -n 1)
+  PASSWORD=$(cat /dev/urandom | tr -dc '[:alnum:]' | fold -w ${1:-25} | head -n 1 | head -c -1)
+
 }
 
 # Create application.conf if it does not exist
